83e8388eac10f69bc6774ac995ccc443cfee38dd
leleiahsia <65791337+leleiahsia@users.noreply.github.com>
2026-07-14 20:16:06 -0400

    Configure Google OAuth web client

--- web/config.js
+++ web/config.js
@@ -1,2 +1,2 @@
 // Replace this value with the Web OAuth client ID from Google Cloud Console.
-window.PSALMS_GOOGLE_CLIENT_ID = "YOUR_WEB_OAUTH_CLIENT_ID.apps.googleusercontent.com";
+window.PSALMS_GOOGLE_CLIENT_ID = "629973082044-89t10s55mj6ut16uj8fn53erqfntagh4.apps.googleusercontent.com";
